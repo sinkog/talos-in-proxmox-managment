2b6fdab4c32860debd4a293518862da0fe8e759a
gabor.sinko <gabor.sinko@sagemcom.hu>
2023-10-09 14:40:00 +0200

    daily commit

--- managed.py
+++ managed.py
@@ -2,28 +2,34 @@ import yaml
 import json
 import paramiko
 import time
-import re
 import subprocess
 
 # Load the contents of the settings.yaml file
 with open('settings.yaml', 'r') as file:
     data = yaml.safe_load(file)
 
+
 def check_pool(ssh_client, cluster):
     vm_command = f"pvesh ls /pools|grep {cluster['name']}"
     stdin, stdout, stderr = ssh_client.exec_command(vm_command)
     output = stdout.read().decode().strip()
     return output
+
+
 def create_pool(ssh_client, cluster):
     vm_command = f"pvesh create /pools --poolid '{cluster['name']}' --comment 'managed by ansible'"
     stdin, stdout, stderr = ssh_client.exec_command(vm_command)
     output = stdout.read().decode().strip()
     return output
+
+
 def delete_pool(ssh_client, cluster):
     vm_command = f"pvesh delete /pools/{cluster['name']}"
     stdin, stdout, stderr = ssh_client.exec_command(vm_command)
     output = stdout.read().decode().strip()
     return output
+
+
 def check_vm(ssh_client, vm):
     vm_command = f"qm status {vm['vm_id']}"
     stdin, stdout, stderr = ssh_client.exec_command(vm_command)
@@ -36,20 +42,24 @@ def check_vm(ssh_client, vm):
     else:
         return 'absent'
 
+
 def check_vm_ip(ssh_client, vm):
-    command = f"grep -Po '(?<=net[0-9]: e1000=)([[:xdigit:]]\\{1,2\\}:)\\{5\\}[[:xdigit:]]\\{1,2\\}' /etc/pve/qemu-server/{vm['vm_id']}.conf"
+    command = "grep -Po '(?<=net[0-9]: e1000=)([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'" \
+              f" /etc/pve/qemu-server/{vm['vm_id']}.conf"
     stdin, stdout, stderr = ssh_client.exec_command(command)
     mac_address = stdout.read().decode().strip()
-    
+
     while True:
         count = 0
-        curl_command = f"curl -k -u ansible:ansible http://10.10.10.1/rest/dhcp-server/lease?mac-address={mac_address}"
+        curl_command = "curl -k -u ansible:ansible" \
+            f" http://10.162.253.1/rest/ip/dhcp-server/lease?mac-address={mac_address}"
         output = subprocess.check_output(curl_command, shell=True).decode().strip()
         response = json.loads(output)
         ip_address = ""
-        if "active-address" in response:
-            vm['ip_address'] = response["acrive-address"]
-            break
+        if response:
+            if "active-address" in response[0]:
+                vm['ip_address'] = response[0]["active-address"]
+                break
 
         # Wait for 5 seconds before checking again
         time.sleep(5)
@@ -58,98 +68,124 @@ def check_vm_ip(ssh_client, vm):
             break
     if vm['ip_address']:
         return True
-    else
+    else:
         return False
 
-# Use curl to query the MikroTik router's REST API for the IP address
-    curl_command = f"curl -k -u ansible:ansible http://10.10.10.1/rest/dhcp-server/lease?mac-address={mac_address}"
-    result = subprocess.run(curl_command, shell=True, capture_output=True, text=True)
-    output = result.stdout.strip()
-
-    # Parse the output to extract the IP address
-    ip_address = ""
-    if "active-address" in output:
-        ip_address = output.split("active-address")[1].split(":")[1].strip().strip('"')
-
-    # Update the IP address in the controlplane object
-    controlplane['ip_address'] = ip_address
-    return True
 
 def create_vm(ssh_client, vm):
     qt_create_command = f"""
-    mkdir -p /var/lib/vz2/images/{vm['vm_id']}
-    qemu-img create - qcow2 /var/lib/vz2/images/{vm['vm_id']}/{vm['vm_id']}.qcow2 {vm['storage']}
+    rm -rf /tmp/{vm['vm_id']}.qcow2 
+    qemu-img create -f qcow2 /tmp/{vm['vm_id']}.qcow2 {vm['storage']}
     qm create {vm['vm_id']} \
       --name {vm['name']} \
       --onboot 1 \
       --net0 e1000,bridge=vmbr0,firewall=1 \
       --cdrom vm-store:iso/metal-amd64.iso,media=cdrom \
-      --sata0 vm-store:/var/lib/vz2/images/{vm['vm_id']}/{vm['vm_id']}.qcow2 \
+      --sata0 vm-store:0,import-from=/tmp/{vm['vm_id']}.qcow2,format=qcow2 \
       --ostype l26 \
       --onboot yes \
-      --start true \
+      --kvm 0 \
+      --cpu EPYC-v3 \
       --memory {vm['memory']} \
-      --socket {vm['cpu']} \
-      --cores {vm['core']} &&
-    while true; do \
-     VM_STATUS=\$(pvesh get /nodes/$VM_NODE/qemu/{vm['vm_id']}/status/current); \
-       if [ "\$VM_STATUS" = "running" ]; then \
-         break; \
-         else \
-           sleep 10; \
-         fi; \
-     done
+      --sockets {vm['cpu']} \
+      --cores {vm['core']} 
     """
-
     stdin, stdout, stderr = ssh_client.exec_command(qt_create_command)
     output = stdout.read().decode().strip()
     return output
 
+
 def delete_vm(ssh_client, vm):
     qt_create_command = f"""
-        qt stop {vm['vm_id']}
+        qm stop {vm['vm_id']}
         qm status {vm['vm_id']}
-        qt destroy {vm['vm_id']} --destroy-unreferenced-disks true --purge true
+        qm destroy {vm['vm_id']} --destroy-unreferenced-disks true --purge true
         """
     stdin, stdout, stderr = ssh_client.exec_command(qt_create_command)
     output = stdout.read().decode().strip()
     return output
 
-def recreate_vm(ssh_client, vm):
-    vm_command = "..."
-    stdin, stdout, stderr = ssh_client.exec_command(vm_command)
+
+def stop_vm(ssh_client, vm):
+    qt_create_command = f"""
+        qm stop {vm['vm_id']}
+        """
+    stdin, stdout, stderr = ssh_client.exec_command(qt_create_command)
     output = stdout.read().decode().strip()
     return output
+
+
+def start_vm(ssh_client, vm):
+    qt_create_command = f"""
+    qm start {vm['vm_id']}
+    while true; do \
+      VM_STATUS=$(qm status {vm['vm_id']}); \
+        if [[ "$VM_STATUS" == *"running"* ]]; then \
+          break; \
+        else \
+          sleep 10; \
+        fi; \
+    done
+    """
+    stdin, stdout, stderr = ssh_client.exec_command(qt_create_command)
+    output = stdout.read().decode().strip()
+    return output
+
 
 def connect_pve(pve):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    private_key= paramiko.RSAKey.from_private_key_file(pve['private_key_path'])
-    ssh.connect(pve['ip_address'], pve['username'], pkey= private_key)
+    private_key = paramiko.RSAKey.from_private_key_file(pve['private_key_path'])
+    ssh.connect(pve['ip_address'], username=pve['username'], pkey=private_key)
     return ssh
 
-def machine_control(data, cluster, node):
+
+def machine_pre_control(data, cluster, node):
     for pve in data['virtualization_servers']:
-        if pve['name'] == node['name']:
-            node['ssh_client']= pve['ssh_client']
+        if pve['name'] == node['server']:
+            node['ssh_client'] = pve['ssh_client']
     if not cluster['configured']:
-        pool_state = check_pool(node.ssh_client, cluster)
+        pool_state = check_pool(node['ssh_client'], cluster)
         if cluster['state'] == 'present' and pool_state == '':
-            create_pool(node.ssh_client, cluster)
+            create_pool(node['ssh_client'], cluster)
         elif cluster['state'] == 'absent':
-            delete_pool(node.ssh, cluster)
+            delete_pool(node['ssh_client'], cluster)
         cluster['configured'] = True
-    node_state = check_vm(node['ssh_client'], node)
-    if node_state == '':
+    node['actual_state'] = check_vm(node['ssh_client'], node)
+    return
+
+def machine_control(data, cluster, node):
+    if node['actual_state'] == 'absent':
         if node['state'] == 'present':
             create_vm(node['ssh_client'], node)
-    else:
-        if node['state'] == 'recreate':
-            delete_vm(node['ssh_client'], node)
+            start_vm(node['ssh_client'], node)
+        if node['state'] == 'stopped':
+            create_vm(node['ssh_client'], node)
+    if node['actual_state'] == 'stopped':
+        if node['state'] == 'present':
+            start_vm(node['ssh_client'], node)
         elif node['state'] == 'absent':
             delete_vm(node['ssh_client'], node)
+    if node['actual_state'] == 'running':
+        if node['state'] == 'absent':
+            stop_vm(node['ssh_client'], node)
+            delete_vm(node['ssh_client'], node)
+        if node['state'] == 'stopped':
+            stop_vm(node['ssh_client'], node)
+    return
+def machine_post_control(data, cluster, node):
+    if node['state'] == 'present':
+        check_vm(node['ssh_client'], node)
+
 
 def cluster_managment(cluster, data):
+    cluster['configured'] = False
+#VM pre work
+    for controlplane in cluster['controlplanes']:
+        machine_pre_control(data, cluster, controlplane)
+    for worker in cluster['workers']:
+        machine_pre_control(data, cluster, worker)
+#VM work
     if cluster['state'] == 'present':
         for controlplane in cluster['controlplanes']:
             machine_control(data, cluster, controlplane)
@@ -169,18 +205,33 @@ def cluster_managment(cluster, data):
             state = node['state']
             node['state'] = 'absent'
             machine_control(data, cluster, node)
+            node['actual_state'] = 'absent'
             node['state'] = state
         for node in cluster['workers']:
             state = node['state']
             node['state'] = cluster['state']
             machine_control(data, cluster, node)
+            node['actual_state'] = 'absent'
             node['state'] = state
         cluster['configured'] = False
         for node in cluster['controlplanes']:
             machine_control(data, cluster, node)
         for node in cluster['workers']:
             machine_control(data, cluster, node)
-    
+    elif cluster['state'] == 'stopped':
+        cluster['force'] = True
+        for node in cluster['controlplanes']:
+            node['state'] = cluster['state']
+            machine_control(data, cluster, node)
+        for node in cluster['workers']:
+            node['state'] = cluster['state']
+            machine_control(data, cluster, node)
+    #VM work post
+    if cluster['state'] == 'present' or cluster['state'] == 'recreate':
+        for controlplane in cluster['controlplanes']:
+            machine_post_control(data, cluster, controlplane)
+        for worker in cluster['workers']:
+            machine_post_control(data, cluster, worker)
 
 
 for pve in data['virtualization_servers']:
@@ -190,9 +241,10 @@ for pve in data['virtualization_servers']:
 for cluster in data['clusters']:
     cluster_managment(cluster, data)
 
-
-
 for pve in data['virtualization_servers']:
     pve['ssh_client'].close()
 
-
+#import talos
+## talos config
+##for cluster in data['clusters']:
+#    talos.talos_managment(cluster, data)
